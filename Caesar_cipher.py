45fcfa5bc1889bbce694f1fbe6f3c6362bb464d9
Fakhri Amin <fakhriamin0506@gmail.com>
2021-07-22 13:22:04 +0900

    Menambahkan 3 file projects

---
+++ Caesar_cipher.py
@@ -0,0 +1,53 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+#! Encode and Decode : Caeser Cipher
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+
+repeat = True
+while repeat == True:
+    direction = input(
+        "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    def Ceaser(start_text, shift_amount, cipher_direction):
+        shift_amount %= 26
+        result = ""
+        new_char = ""
+        if cipher_direction == "decode":
+            shift_amount = -shift_amount
+        for i in start_text:
+            if i.isalpha():
+                position = alphabet.index(i)
+                new_position = position + shift_amount
+                new_char = alphabet[new_position]
+            else:
+                new_char = i
+            result += new_char
+
+        print(f"The {cipher_direction}d text : {Ceaser(text, shift, direction)}")
+        return result
+
+    reset = input("Do you want to do it again?\n")
+    if reset.lower() == "no":
+        repeat = False
